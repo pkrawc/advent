7954dd17c1db94700e501c497b7346abac0ea560
Patrick Krawczykowski <patrick@dreadful.design>
2020-12-02 23:48:43 -0600

    day 3

--- src/day_03/index.js
+++ src/day_03/index.js
@@ -1,30 +1,99 @@
 const { readInput, test } = require("../utils")
 
-const prepareInput = (rawInput) => rawInput
+const rawTestInput = `
+..##.......
+#...#...#..
+.#....#..#.
+..#.#...#.#
+.#...##..#.
+..#.##.....
+.#.#.#....#
+.#........#
+#.##...#...
+#...##....#
+.#..#...#.#
+`
+
+const prepareInput = (rawInput) =>
+  rawInput
+    .trim()
+    .split(/\n/)
+    .map((line) => line.split(""))
 
 const input = prepareInput(readInput())
 
+function getArea(neededWidth, input) {
+  const inputWidth = input[0].length
+  return input.map((line) => {
+    const repeat = neededWidth / inputWidth
+    const newLine = []
+    for (let i = 0; i <= repeat; i++) {
+      newLine.push(...line)
+    }
+    return newLine
+  })
+}
+
+function getTrees(slope, input) {
+  const coords = {
+    x: 0,
+    y: 0,
+    trees: 0,
+  }
+  const height = input.length
+  const needed = Math.round((height / slope.y) * slope.x)
+  const field = getArea(needed, input)
+  while (coords.x < needed) {
+    const { x, y } = coords
+    const isTree = field[y][x] === "#"
+    coords.x += slope.x
+    coords.y += slope.y
+    if (isTree) coords.trees++
+  }
+  return coords.trees
+}
+
 function partOne(input) {
-  return
+  const trees = getTrees({ x: 3, y: 1 }, input)
+  return trees
 }
 
 function partTwo(input) {
-  return
+  const slopes = [
+    { x: 1, y: 1 },
+    { x: 3, y: 1 },
+    { x: 5, y: 1 },
+    { x: 7, y: 1 },
+    { x: 1, y: 2 },
+  ]
+  const aggTrees = slopes.map((slope) => {
+    return getTrees(slope, input)
+  })
+
+  return aggTrees.reduce((a, b) => a * b)
 }
 
 /* Tests */
 
-// test(result, expected)
+const testInput = prepareInput(rawTestInput)
+
+const testResult = partOne(testInput)
+
+test(testResult, 7)
 
 /* Results */
 
-console.time("Part One Time")
+console.log(`-------------`)
+console.time("Part One")
 const partOneResult = partOne(input)
-console.timeEnd("Part One Time")
-
-console.time("Part Two Time")
-const partTwoResult = partTwo(input)
-console.timeEnd("Part Two Time")
-
+console.timeEnd("Part One")
 console.log("Solution to part 1: ", partOneResult)
+console.log(`-------------`)
+
+console.log(`-------------`)
+console.time("Part Two")
+const partTwoResult = partTwo(input)
+console.timeEnd("Part Two")
+
 console.log("Solution to part 2: ", partTwoResult)
+console.log(`-------------`)
