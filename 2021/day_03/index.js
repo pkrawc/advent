c34822d002e42681c5496c6180f7669458426406
Patrick Krawczykowski <patrick@dreadful.design>
2021-12-05 13:02:43 -0600

    update day 03

--- 2021/day_03/index.js
+++ 2021/day_03/index.js
@@ -5,28 +5,24 @@ const prepareInput = (rawInput) =>
 
 const input = prepareInput(readInput())
 
-const getCommonMap = (array) => {
-  const commonMap = [
-    ...array.reduce(
-      (map, item) => map.set(item, (map.get(item) || 0) + 1),
-      new Map()
-    ),
-  ]
-  return commonMap.sort((a, b) => b[1] - a[1]).map((i) => i[0])
+// Logic pretty much stolen from Maddie.
+const getBitFrequency = (array, idx) => {
+  let zeros = 0
+  let ones = 0
+  array.forEach((item) => {
+    if (item[idx] === "0") zeros++
+    else ones++
+  })
+  return zeros > ones ? "0" : "1"
 }
 
 function partOne(input, bitLength = 12) {
   const gammaNum = []
   const epsilonNum = []
   for (let i = 0; i < bitLength; i++) {
-    const bitArray = []
-    const bitDict = {}
-    for (let x = 0; x < input.length; x++) {
-      bitArray.push(input[x][i])
-    }
-    const [most, least] = getCommonMap(bitArray)
+    const most = getBitFrequency(input, i)
     gammaNum.push(most)
-    epsilonNum.push(least)
+    epsilonNum.push(most === "1" ? "0" : "1")
   }
   const gamma = parseInt(gammaNum.join(""), 2)
   const epsilon = parseInt(epsilonNum.join(""), 2)
@@ -36,31 +32,20 @@ function partOne(input, bitLength = 12) {
 function partTwo(oInput) {
   let idx = 0
   let input = oInput
-  while (input.length > 2) {
-    const bitArray = []
-    for (let i = 0; i < input.length; i++) {
-      bitArray.push(input[i][idx])
-    }
-    const [most] = getCommonMap(bitArray)
+  while (input.length > 1) {
+    const most = getBitFrequency(input, idx)
     input = input.filter((item) => item[idx] === most)
     idx++
   }
-  const oxygen = parseInt(input.find((num) => num[idx] === "1").join(""), 2)
+  const oxygen = parseInt(input[0].join(""), 2)
   idx = 0
   input = oInput
-  while (input.length > 2) {
-    const bitArray = []
-    for (let i = 0; i < input.length; i++) {
-      bitArray.push(input[i][idx])
-    }
-    const [_, least] = getCommonMap(bitArray)
-    input = input.filter((item) => item[idx] === least)
+  while (input.length > 1) {
+    const most = getBitFrequency(input, idx)
+    input = input.filter((item) => item[idx] !== most)
     idx++
   }
-  const carbonDioxide = parseInt(
-    input.find((num) => num[idx] === "0").join(""),
-    2
-  )
+  const carbonDioxide = parseInt(input[0].join(""), 2)
   return oxygen * carbonDioxide
 }
 
