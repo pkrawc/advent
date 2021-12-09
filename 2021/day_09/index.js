4925bdd9e68d487c19c83fb280678e9dbadc323b
Patrick Krawczykowski <patrick@dreadful.design>
2021-12-09 11:00:39 -0600

    day 8 part 1

---
+++ 2021/day_09/index.js
@@ -0,0 +1,35 @@
+const { readInput, test } = require("../../utils")
+
+const prepareInput = (rawInput) => rawInput
+
+const input = prepareInput(readInput())
+
+function partOne(input) {
+  return
+}
+
+function partTwo(input) {
+  return
+}
+
+/* Tests */
+
+// test(result, expected)
+
+/* Results */
+
+console.log("-----------------")
+
+console.time("Part One Time")
+const partOneResult = partOne(input)
+console.timeEnd("Part One Time")
+console.log("Solution to part 1: ", partOneResult)
+
+console.log("-----------------")
+
+console.time("Part Two Time")
+const partTwoResult = partTwo(input)
+console.timeEnd("Part Two Time")
+console.log("Solution to part 2: ", partTwoResult)
+
+console.log("-----------------")
