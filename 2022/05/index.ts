9cddcbec6edbf70dfccf9d3b57200118365d2c27
pkrawc <patrick@dreadful.design>
2022-12-05 10:13:58 -0600

    complete 2022 05

--- 2022/05/index.ts
+++ 2022/05/index.ts
@@ -14,27 +14,43 @@ const input = prepareInput(readInput())
 // [S] [N] [F] [G] [W] [B] [H] [F] [N]
 //  1   2   3   4   5   6   7   8   9
 
-const stacks = new Map([
-  [1, ["S", "Z", "P", "D", "L", "B", "F", "C"]],
-  [2, ["N", "V", "G", "P", "H", "W", "B"]],
-  [3, ["F", "W", "B", "J", "G"]],
-  [4, ["G", "J", "N", "F", "L", "W", "C", "S"]],
-  [5, ["W", "J", "L", "T", "P", "M", "S", "H"]],
-  [6, ["B", "C", "W", "G", "F", "S"]],
-  [7, ["H", "T", "P", "M", "Q", "B", "W"]],
-  [8, ["F", "S", "W", "T"]],
-  [9, ["N", "C", "R"]],
-])
-
-function rearrange(input: string[]) {
+function rearrange9000(input: string[], stacks) {
   const [start, insString] = input
   const instructions = insString.split(/\n/)
   let result = ""
 
   for (let instruction of instructions) {
-    const [itemCount, movement] = instruction.split(" from ")
-    const [from, to] = movement.split(" to ")
-    for (let i = 0; 0 < parseInt(itemCount); i++) {}
+    const [countMatch, from, to] = instruction.matchAll(/\d+/g)
+    const count = parseInt(countMatch[0])
+    const fromStack = stacks[parseInt(from[0]) - 1]
+    const toStack = stacks[parseInt(to[0]) - 1]
+    for (let i = 0; i < count; i++) {
+      const item = fromStack.pop()
+      toStack.push(item)
+    }
+  }
+  stacks.forEach((stack: string[]) => {
+    result += stack.pop()
+  })
+  return result
+}
+
+function rearrange9001(input: string[], stacks) {
+  const [start, insString] = input
+  const instructions = insString.split(/\n/)
+  let result = ""
+
+  for (let instruction of instructions) {
+    const [countMatch, from, to] = instruction.matchAll(/\d+/g)
+    const items = []
+    const count = parseInt(countMatch[0])
+    const fromStack = stacks[parseInt(from[0]) - 1]
+    const toStack = stacks[parseInt(to[0]) - 1]
+    for (let i = 0; i < count; i++) {
+      const item = fromStack.pop()
+      items.push(item)
+    }
+    toStack.push(...items.reverse())
   }
   stacks.forEach((stack: string[]) => {
     result += stack.pop()
@@ -43,12 +59,35 @@ function rearrange(input: string[]) {
 }
 
 function partOne(input) {
-  const rearraged = rearrange(input)
+  const stacks = [
+    ["S", "Z", "P", "D", "L", "B", "F", "C"],
+    ["N", "V", "G", "P", "H", "W", "B"],
+    ["F", "W", "B", "J", "G"],
+    ["G", "J", "N", "F", "L", "W", "C", "S"],
+    ["W", "J", "L", "T", "P", "M", "S", "H"],
+    ["B", "C", "W", "G", "F", "S"],
+    ["H", "T", "P", "M", "Q", "B", "W"],
+    ["F", "S", "W", "T"],
+    ["N", "C", "R"],
+  ]
+  const rearraged = rearrange9000(input, stacks)
   return rearraged
 }
 
 function partTwo(input) {
-  return input
+  const stacks = [
+    ["S", "Z", "P", "D", "L", "B", "F", "C"],
+    ["N", "V", "G", "P", "H", "W", "B"],
+    ["F", "W", "B", "J", "G"],
+    ["G", "J", "N", "F", "L", "W", "C", "S"],
+    ["W", "J", "L", "T", "P", "M", "S", "H"],
+    ["B", "C", "W", "G", "F", "S"],
+    ["H", "T", "P", "M", "Q", "B", "W"],
+    ["F", "S", "W", "T"],
+    ["N", "C", "R"],
+  ]
+  const output = rearrange9001(input, stacks)
+  return output
 }
 
 /* Tests */
@@ -86,11 +125,11 @@ console.log("Solution to part 1: ", partOneResult)
 
 console.log("-----------------")
 
-// console.time("Part Two Time")
-// const partTwoResult = partTwo(input)
-// console.timeEnd("Part Two Time")
-// console.log("Solution to part 2: ", partTwoResult)
+console.time("Part Two Time")
+const partTwoResult = partTwo(input)
+console.timeEnd("Part Two Time")
+console.log("Solution to part 2: ", partTwoResult)
 
-// console.log("-----------------")
+console.log("-----------------")
 
 export {}
