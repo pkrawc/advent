86f98edd3279a70faba3f677439d5e30b942485b
pkrawc <patrick@dreadful.design>
2022-12-06 10:05:11 -0600

    2022 06

---
+++ 2022/06/index.ts
@@ -0,0 +1,54 @@
+import { readInput, test } from "@utils"
+import { combination } from "generatorics"
+
+const prepareInput = (rawInput: string) => rawInput.split("")
+
+const input = prepareInput(readInput())
+
+function findMarker(chars: string[], markerLength: number) {
+  let step = markerLength
+  let marker = false
+  while (!marker) {
+    const test = new Set(chars.slice(step - markerLength, step))
+    if (test.size === markerLength) {
+      marker = true
+      break
+    }
+    step++
+  }
+  return step
+}
+
+function partOne(input) {
+  const stepCount = findMarker(input, 4)
+  return stepCount
+}
+
+function partTwo(input) {
+  const stepCount = findMarker(input, 14)
+  return stepCount
+}
+
+/* Tests */
+
+// test(result, expected)
+
+/* Results */
+
+console.log("-----------------")
+
+console.time("Part One Time")
+const partOneResult = partOne(input)
+console.timeEnd("Part One Time")
+console.log("Solution to part 1: ", partOneResult)
+
+console.log("-----------------")
+
+console.time("Part Two Time")
+const partTwoResult = partTwo(input)
+console.timeEnd("Part Two Time")
+console.log("Solution to part 2: ", partTwoResult)
+
+console.log("-----------------")
+
+export {}
