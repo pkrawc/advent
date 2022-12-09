bcbf24b5546eb0c41b451dc52cd87138d1e520dc
Patrick Krawczykowski <patrick@dreadful.design>
2022-12-09 08:42:57 -0600

    2022 09 start

---
+++ 2022/09/index.ts
@@ -0,0 +1,119 @@
+import { readInput, test } from "@utils"
+
+const prepareInput = (rawInput: string) =>
+  rawInput.split(/\n/).map((l) => l.split(" "))
+
+// const input = prepareInput(readInput())
+
+function moveDirection(movement, axis, steps, hx, hy, tx, ty, set) {
+  for (let step = 1; step < steps; step++) {
+    hx = hx + 1
+    // Move Diagonally
+    if (ty !== hy && tx !== ty) {
+      tx = tx + 1
+      ty = hx - tx
+    } else {
+      tx = tx + 1
+    }
+    set.add(`${tx},${ty}`)
+  }
+}
+
+function mapTailMovement(input) {
+  const visited = new Set()
+  let [tx, ty] = [0, 0]
+  let [hx, hy] = [0, 0]
+  for (let [direction, steps] of input) {
+    console.log({ direction, steps })
+    switch (direction) {
+      case "R": {
+        for (let step = 0; step < steps; step++) {
+          hx = hx + 1
+          // Move Diagonally
+          if (ty !== hy && tx !== ty) {
+            tx = tx + 1
+            ty = hx - tx
+          } else {
+            tx = tx + 1
+          }
+          visited.add(`${tx},${ty}`)
+        }
+      }
+      case "U": {
+        for (let step = 0; step < steps; step++) {
+          hy = hy - 1
+          if (ty !== hy && tx !== ty) {
+            tx = hx - tx
+            ty = ty - 1
+          } else {
+            ty = ty - 1
+          }
+          visited.add(`${tx},${ty}`)
+        }
+      }
+      case "L": {
+        for (let step = 0; step < steps; step++) {
+          hx = hx - 1
+          if (tx !== hx && ty !== hy) {
+            tx = tx - 1
+            ty = hy - ty
+          } else {
+            tx = tx - 1
+          }
+          visited.add(`${tx},${ty}`)
+        }
+      }
+      case "D": {
+        for (let step = 0; step < steps; step++) {
+          hy = hy + 1
+          if (ty !== hy && tx !== ty) {
+            tx = hx - tx
+            ty = ty + 1
+          } else {
+            ty = ty + 1
+          }
+          visited.add(`${tx},${ty}`)
+        }
+      }
+    }
+  }
+  return visited
+}
+
+// function partOne(input) {
+//   return input
+// }
+
+// function partTwo(input) {
+//   return input
+// }
+
+/* Tests */
+
+const testInput = prepareInput(readInput("test-input.txt"))
+
+const result = mapTailMovement(testInput)
+
+console.log(result)
+
+test(result.size, 13)
+
+/* Results */
+
+// console.log("-----------------")
+
+// console.time("Part One Time")
+// const partOneResult = partOne(input)
+// console.timeEnd("Part One Time")
+// console.log("Solution to part 1: ", partOneResult)
+
+// console.log("-----------------")
+
+// console.time("Part Two Time")
+// const partTwoResult = partTwo(input)
+// console.timeEnd("Part Two Time")
+// console.log("Solution to part 2: ", partTwoResult)
+
+// console.log("-----------------")
+
+export {}
