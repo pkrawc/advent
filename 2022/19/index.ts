b85c9ec5bebe0a25a6150d0e8ebe7ff092a2fd0e
pkrawc <patrick@dreadful.design>
2023-11-28 09:47:26 -0600

    2022 D19 D20

--- 2022/19/index.ts
+++ 2022/19/index.ts
@@ -5,18 +5,83 @@ const prepareInput = (rawInput: string) => rawInput.split(/\n/)
 // const input = prepareInput(readInput())
 const testInput = prepareInput(readInput("test-input.txt"))
 
-function parseBluePrints(input) {
+function parseBlueprints(input) {
   return input.map((line) => {
-    // const [id, ore, clayBit, obsidianBit, geodeBit] =
-    //   line.split(/:\s|\.\s/)
-    // const [id] = idBit.match(/(\d+)/g)
-    // const [ore] = oreBit.match(/(\d+)/g)
-    // return { id, ore }
+    const [id, ...costs] = line.match(/(\d+)/g)
+
+    const [ore, clayOre, obsidianOre, obsidianClay, geodeOre, geodeObsidian] =
+      costs.map(Number)
+
+    return {
+      id,
+      ore,
+      clayOre,
+      obsidianOre,
+      obsidianClay,
+      geodeOre,
+      geodeObsidian,
+    }
   })
 }
 
 function partOne(input) {
-  return input
+  const totals = []
+  const blueprints = parseBlueprints(input)
+  for (const blueprint of blueprints) {
+    let minute = 1
+    const materials = {
+      geode: 0,
+      obsidian: 0,
+      clay: 0,
+      ore: 0,
+    }
+    const bots = {
+      geode: 0,
+      obsidian: 0,
+      clay: 0,
+      ore: 1,
+    }
+    while (minute < 10) {
+      const newBots = {
+        geode: 0,
+        obsidian: 0,
+        clay: 0,
+        ore: 0,
+      }
+      if (
+        blueprint.geodeOre <= materials.ore &&
+        blueprint.geodeObsidian <= materials.obsidian
+      ) {
+        materials.ore -= blueprint.geodeOre
+        materials.obsidian -= blueprint.geodeObsidian
+        newBots.geode++
+      }
+      if (
+        blueprint.obsidianOre <= materials.ore &&
+        blueprint.obsidianClay <= materials.clay
+      ) {
+        materials.ore -= blueprint.obsidianOre
+        materials.clay -= blueprint.obsidianClay
+        newBots.obsidian++
+      }
+      if (blueprint.clayOre <= materials.ore) {
+        materials.ore -= blueprint.clayOre
+        newBots.clay++
+      }
+      if (blueprint.ore <= materials.ore) {
+        materials.ore -= blueprint.ore
+        newBots.ore++
+      }
+      // if (Object.values(newBots).some(Boolean)) minute++
+      for (const [key, count] of Object.entries(bots)) {
+        materials[key] += count
+        bots[key] = count + newBots[key]
+      }
+      console.log({ bots, materials, minute })
+      minute++
+    }
+  }
+  return totals
 }
 
 function partTwo(input) {
@@ -25,7 +90,7 @@ function partTwo(input) {
 
 /* Tests */
 
-const result = parseBluePrints(testInput)
+const result = partOne(testInput)
 
 console.log(result)
 
